357e7a5a25862de90c2b507371a178904bbb9cb1
witer33 <34513257+witer33@users.noreply.github.com>
2022-05-16 20:35:31 +0200

    Added comments

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ var challengeTemplate string
 
 const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+// Generates a random string using crypto/rand.
 func randString(length int) (string, error) {
 	bytes := make([]byte, length)
 
@@ -32,24 +33,33 @@ func randString(length int) (string, error) {
 	return string(bytes), nil
 }
 
+// Config is the Middleware Config.
 type Config struct {
 	Filter      func(*fiber.Ctx) bool
 	PowInterval time.Duration
 	Difficulty  int
 }
 
+// this stores individual information for an ip address.
 type ipStatus struct {
-	verified     bool
+	// if verified is true skip the pow check
+	verified bool
+	// secretNumber is the number that the user has to find
 	secretNumber int
+	// secretSuffix is the hash suffix
 	secretSuffix string
-	hash         string
+	// hash is the hash given to the user
+	hash string
 }
 
+// New initialize the middleware with the default config or a custom one.
 func New(config ...Config) fiber.Handler {
 	var cfg Config
 	if len(config) > 0 {
 		cfg = config[0]
 	}
+
+	// Set default config.
 
 	if cfg.PowInterval == 0 {
 		cfg.PowInterval = time.Second * 20
@@ -59,17 +69,22 @@ func New(config ...Config) fiber.Handler {
 		cfg.Difficulty = 30000
 	}
 
+	// ipStatus storage.
 	ipCache := cache.New(cfg.PowInterval, 10*time.Second)
 
+	// Middleware handler.
 	return func(c *fiber.Ctx) error {
+		// Handles Config.Filter.
 		if cfg.Filter != nil && cfg.Filter(c) {
 			return c.Next()
 		}
 
+		// Checks if ipStatus is already present for this ip.
 		statusInterface, found := ipCache.Get(c.IP())
 		var status *ipStatus
 
 		if !found {
+			// Generates a new ipStatus for this ip.
 			secretNumber, err := rand.Int(rand.Reader, big.NewInt(int64(cfg.Difficulty)))
 			if err != nil {
 				return err
@@ -87,6 +102,7 @@ func New(config ...Config) fiber.Handler {
 				secretSuffix: secretSuffix,
 			}
 
+			// Generates the hash for the user.
 			byteHash := sha256.Sum256([]byte(fmt.Sprintf("%d-%s", status.secretNumber, status.secretSuffix)))
 			status.hash = hex.EncodeToString(byteHash[:])
 			ipCache.Set(c.IP(), status, cache.DefaultExpiration)
@@ -94,10 +110,12 @@ func New(config ...Config) fiber.Handler {
 			status = statusInterface.(*ipStatus)
 		}
 
+		// Skips already verified ip.
 		if status.verified {
 			return c.Next()
 		}
 
+		// Checks if the user already solved the challenge.
 		secretCookie := c.Cookies("_challenge_n", "-1")
 		secretNumber, err := strconv.Atoi(secretCookie)
 		if err != nil {
@@ -109,6 +127,7 @@ func New(config ...Config) fiber.Handler {
 			return c.Next()
 		}
 
+		// Renders the challenge template.
 		c.Set(fiber.HeaderContentType, fiber.MIMETextHTMLCharsetUTF8)
 		return c.SendString(fmt.Sprintf(challengeTemplate, status.hash, status.secretSuffix))
 	}
